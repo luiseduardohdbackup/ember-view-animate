732f55a42d799f9e222135a65da2faa5db1225a5
Adam Kirk <atomkirk@gmail.com>
2014-01-30 08:09:56 -0700

    Create view_animation_mixin.js

---
+++ view_animation_mixin.js
@@ -0,0 +1,28 @@
+App.ViewAnimationMixin = Ember.Mixin.create({
+
+  _VAM_didInsertElement: (function() {
+    if (this.needsAnimateIn) {
+      this.$().hide();
+      return this.needsAnimateIn(this.$());
+    }
+  }).on('didInsertElement'),
+  
+  _VAM_willDestroyElement: (function() {
+    var $this, clone, idx, parent,
+      _this = this;
+    if (this.needsAnimateOut) {
+      $this = this.$();
+      clone = $this.clone();
+      clone.find('script').remove();
+      parent = $this.parent();
+      idx = parent.children().index($this);
+      return Ember.run.scheduleOnce('afterRender', function() {
+        $(parent.children()[idx]).after(clone);
+        return _this.needsAnimateOut(clone, function() {
+          return clone.remove();
+        });
+      });
+    }
+  }).on('willDestroyElement')
+  
+});
